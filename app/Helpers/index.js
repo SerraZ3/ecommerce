c6db4fd58fda66e6c15e9f8ef6eb9996469cac2c
SerraZ <SerraZ3>
2019-10-06 19:46:47 -0300

    Create helpers to file

--- app/Helpers/index.js
+++ app/Helpers/index.js
@@ -16,7 +16,7 @@ const str_random = async (length = 40) => {
   if (len < length) {
     let size = length - len
     let bytes = await crypto.randomBytes(size)
-    let buffer = new Buffer(bytes)
+    let buffer = Buffer.from(bytes)
     string += buffer
       .toString('base64')
       .replace(/[^a-zA-Z0-9]/g, '')
@@ -24,6 +24,58 @@ const str_random = async (length = 40) => {
   }
   return string
 }
+/**
+ * Move um unico arquivo para o caminho especificado, se nenhum for especificado entao o public/uploads será utilizado
+ *
+ * @param {FileJar} file arquvio a ser gerenciado
+ * @param {string} path caminho onde o arquvio deve ser movido
+ * @return {Object<fileJar>}
+ */
+const manage_single_upload = async (file, path = null) => {
+  path ? path : Helpers.publicPath('uploads')
+
+  // Gera nome aleatorio
+  const random_name = await str_random(30)
+  let filename = `${new Date().getTime()}-${random_name}.${file.subtpe}`
+
+  // Renomeia o arquivo e move ele para o path
+  await file.move(path, { name: filename })
+
+  return file
+}
+
+/***
+ * Move um mulitplos arquivo para o caminho especificado, se nenhum for especificado entao o public/uploads será utilizado
+ * @param {FileJar} fileJar arquvio a ser gerenciado
+ * @param {string} path caminho onde o arquvio deve ser movido
+ */
+const manage_multiple_upload = async (file, path = null) => {
+  path ? path : Helpers.publicPath('uploads')
+
+  let successes = [],
+    errors = []
+
+  await Promise.all(
+    fileJar.files.map(async file => {
+      let random_name = await str_random(30)
+      let filename = `${new Date().getTime()}-${random_name}.${file.subtpe}`
+
+      // Renomeia o arquivo e move ele para o path
+      await file.move(path, { name: filename })
+
+      if (file.moved()) {
+        successes.push(file)
+      } else {
+        errors.push(file)
+      }
+    })
+  )
+
+  return { successes, errors }
+}
+
 module.exports = {
-  str_random
+  str_random,
+  manage_single_upload,
+  manage_multiple_upload
 }
