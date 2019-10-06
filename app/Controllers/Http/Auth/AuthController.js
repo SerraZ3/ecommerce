ae0ec7fee3c4717ce3578c7ad9f65f4cfda1d285
SerraZ <SerraZ3>
2019-10-06 16:36:44 -0300

    AuthController: Registre user

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -1,7 +1,25 @@
 'use strict'
+const Database = use('Database')
+const User = use('App/Models/User')
+const Role = use('Role')
 
 class AuthController {
-  async register({ request, response }) {}
+  async register({ request, response }) {
+    // Criando uma transaction (Serve para cadastrar diversos elementos no DB e garantir que ou todos serão cadastrados ou nenhum)
+    const trx = await Database.beginTransaction()
+    try {
+      const { name, surname, email, password } = request.all()
+      const user = await User.create({ name, surname, email, password }, trx)
+      const userRole = await Role.findBy('slug', 'client')
+      await user.role().attach([userRole.id], null, trx)
+      // Roda todos os valores da trx e garante a persistencia
+      await trx.commit()
+      return response.status(201).send({ data: user })
+    } catch (error) {
+      await trx.rollback()
+      return response.status(400).send({ message: 'Erro ao realizar cadastro' })
+    }
+  }
   async login({ request, response, auth }) {}
   async refresh({ request, response, auth }) {}
   async logout({ request, response, auth }) {}
