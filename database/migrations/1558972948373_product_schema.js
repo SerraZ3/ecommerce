0d9386f601259246734fd82a7dea6a8376a5cb67
SerraZ <SerraZ3>
2019-10-06 01:44:39 -0300

    Category and product migraton

--- database/migrations/1558972948373_product_schema.js
+++ database/migrations/1558972948373_product_schema.js
@@ -21,26 +21,42 @@ class ProductSchema extends Schema {
     })
 
     this.create('image_product', table => {
-
       table.increments()
       table.integer('image_id').unsigned()
       table.integer('product_id').unsigned()
-      
+
       table
         .foreign('image_id')
         .references('id')
         .inTable('images')
         .onDelete('cascade')
-        
+
       table
         .foreign('product_id')
         .references('id')
         .inTable('products')
         .onDelete('cascade')
     })
+    this.create('category_product', table => {
+      table.increments()
+      table.integer('product_id').unsigned()
+      table.integer('category_id').unsigned()
+
+      table
+        .foreign('product_id')
+        .references('id')
+        .inTable('products')
+        .onDelete('cascade')
+      table
+        .foreign('category_id')
+        .references('id')
+        .inTable('categories')
+        .onDelete('cascade')
+    })
   }
 
   down() {
+    this.drop('category_products')
     this.drop('image_products')
     this.drop('products')
   }
