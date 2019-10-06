ede15eef50a420d556597a14e1dced07b5766600
SerraZ <SerraZ3>
2019-10-06 17:17:08 -0300

    All authControler testing and ok

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -10,19 +10,31 @@ class AuthController {
     try {
       const { name, surname, email, password } = request.all()
       const user = await User.create({ name, surname, email, password }, trx)
+
       const userRole = await Role.findBy('slug', 'client')
-      await user.role().attach([userRole.id], null, trx)
+
+      await user.roles().attach([userRole.id], null, trx)
       // Roda todos os valores da trx e garante a persistencia
       await trx.commit()
+
       return response.status(201).send({ data: user })
     } catch (error) {
       await trx.rollback()
+      // console.log(error)
+
       return response.status(400).send({ message: 'Erro ao realizar cadastro' })
     }
   }
   async login({ request, response, auth }) {
     const { email, password } = request.all()
     let data = await auth.withRefreshToken().attempt(email, password)
+    /**
+     *
+     * teste com login
+     * "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjM1LCJpYXQiOjE1NzAzOTI4ODh9.tGJlxqoN3y8liFQ47yjXYYT3zb1ev4VMP-c_yLNPEhM",
+        "refreshToken": "e083c6dab485cf69962b7a514c0e0d7c1kNGoyehWPC+b8HErhsLH+h7LQsg3SKoH2phiNxQZfJ1t51aV2k7R2MZRJ4WKAso"
+     *
+     */
 
     return response.send({ data })
   }
--- start/routes/auth.js
+++ start/routes/auth.js
@@ -3,7 +3,7 @@ const Route = use('Route')
 
 // Agrupar rodas
 Route.group(() => {
-  Route.post('registrer', 'AuthController.register').as('auth.registrer')
+  Route.post('register', 'AuthController.register').as('auth.register')
   Route.post('login', 'AuthController.login').as('auth.login')
   Route.post('refresh', 'AuthController.refresh').as('auth.refresh')
   Route.post('logout', 'AuthController.logout').as('auth.logout')
